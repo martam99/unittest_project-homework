6809535df960b0d6590d337863a46c28551cf906
Mariam Tamrazyan <mart76901@gmail.com>
2023-05-13 19:37:08 +0400

    changes added

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,7 +6,6 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 0, "test"), 1)
-        self.assertEqual(arrs.get([], 7, "test"), "test")
         self.assertEqual(arrs.get([1, 2, 3, 4, 5, 6, 7, 8, 9], 5, "Thereisnothing"), 6)
         self.assertEqual(arrs.get([1, 2, 3, 4, 5], 87, "test"), 'test')
         self.assertEqual(arrs.get([1, 2, 3, 4, 5, 6, 7, 8, 9], -5, "Thereisnothing"), 'Thereisnothing')
