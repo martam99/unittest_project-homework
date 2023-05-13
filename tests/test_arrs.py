b7ff6841880bd2c4a878bdbd3f3150976b7e4753
Mariam Tamrazyan <mart76901@gmail.com>
2023-05-13 19:06:05 +0400

    changes added

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,9 +5,15 @@ from utils import arrs
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], 0, "test"), 1)
+        self.assertEqual(arrs.get([], 7, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3, 4, 5, 6, 7, 8, 9], 5, "Thereisnothing"), 6)
+        self.assertEqual(arrs.get([1, 2, 3, 4, 5], 87, "test"), 'test')
+        self.assertEqual(arrs.get([1], 0, "test"), 1)
+        self.assertEqual(arrs.get([1, 2, 3, 4, 5, 6, 7, 8, 9], -5, "Thereisnothing"), 'Thereisnothing')
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -1), [7])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5, 6, 7], 0), [1, 2, 3, 4, 5, 6, 7])
