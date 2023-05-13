b53af069998b05ef97d06536f1284882171b1b71
Mariam Tamrazyan <mart76901@gmail.com>
2023-05-13 19:29:07 +0400

    changes added

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,7 +11,7 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
+    if index < 0 or index > len(array):
         return default
 
     return array[index]
@@ -38,7 +38,7 @@ def my_slice(coll, start=0, end=None):
 
     if normalized_start < 0:
         if normalized_start < -length:
-            normalized_start = 0
+            normalized_start += 0
         else:
             normalized_start += length
 
